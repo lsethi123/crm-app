a41a1d3bb0a264ee5385a1cb5047c10c2f0971cd
lsethi123 <lalitsethi2003@gmail.com>
2014-08-29 20:48:38 +0530

    added columns back

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+	def nav_link(*args, &block)
+    if block_given?
+      options      = args.first || {}
+      html_options = args.second
+      nav_link(capture(&block), options, html_options)
+    else
+      name         = args[0]
+      options      = args[1] || {}
+      html_options = args[2]
+
+      html_options = convert_options_to_data_attributes(options, html_options)
+      url = url_for(options)
+
+      class_name = current_page?(url) ? 'active' : nil
+
+      href = html_options['href']
+      tag_options = tag_options(html_options)
+
+      href_attr = "href=\"#{ERB::Util.html_escape(url)}\"" unless href
+      "<li class=\"#{class_name}\"><a #{href_attr}#{tag_options}>#{ERB::Util.html_escape(name || url)}</a></li>".html_safe
+    end
+  end
 end
